3b70a371250c57007e301ff585e492b85c02949f
nadia800 <147506022+nadiaTalbi@users.noreply.github.com>
2023-11-24 15:29:13 +0100

    create channel

--- scripts/orderer.sh
+++ scripts/orderer.sh
@@ -6,4 +6,4 @@ export ORDERER_ADMIN_TLS_SIGN_CERT=${PWD}/organizations/crypto-config/ordererOrg
 export ORDERER_ADMIN_TLS_PRIVATE_KEY=${PWD}/organizations/crypto-config/ordererOrganizations/orderer/config.orderers/tls/server.key /dev/null 2>&1
 
 # To join orderer to a channel
-osnadmin channel join --channelID mychannel --config-block ../channel-artifacts/mychannel.block -o localhost:1100 --ca-file "$ORDERER_CA" --client-cert "$ORDERER_ADMIN_TLS_SIGN_CERT" --client-key "$ORDERER_ADMIN_TLS_PRIVATE_KEY" >> log.txt 2>&1
+osnadmin channel join --channelID mychannel --config-block ./channel-artifacts/mychannel.block -o localhost:1100 --ca-file "$ORDERER_CA" --client-cert "$ORDERER_ADMIN_TLS_SIGN_CERT" --client-key "$ORDERER_ADMIN_TLS_PRIVATE_KEY" >> log.txt 2>&1
