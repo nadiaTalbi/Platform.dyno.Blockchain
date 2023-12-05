76851c7b38ed30be0a91cda5b687b4362158107f
nadia800 <147506022+nadiaTalbi@users.noreply.github.com>
2023-12-05 11:30:03 +0100

    Update setAnchorPeer.sh

--- scripts/setAnchorPeer.sh
+++ scripts/setAnchorPeer.sh
@@ -33,7 +33,7 @@ createAnchorPeerUpdate() {
 }
 
 updateAnchorPeer() {
-  peer channel update -o localhost:7050 --ordererTLSHostnameOverride localhost -c mychannel -f dynoAnchors.tx --tls --cafile "$ORDERER_CA" >&log.txt
+  peer channel update -o orderer.example.com:7050 --ordererTLSHostnameOverride orderer.example.com -c mychannel -f dynoAnchors.tx --tls --cafile "$ORDERER_CA" >&log.txt
   res=$?
   cat log.txt
   verifyResult $res "Anchor peer update failed"
