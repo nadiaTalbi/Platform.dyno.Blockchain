9b627410fb4c85d7f3576ef3eb7b14949f3c2a74
nadia800 <147506022+nadiaTalbi@users.noreply.github.com>
2023-12-14 12:00:04 +0100

    update

--- scripts/orderer.sh
+++ scripts/orderer.sh
@@ -6,7 +6,7 @@ export ORDERER_ADMIN_TLS_SIGN_CERT=${PWD}/organizations/ordererOrganizations/exa
 export ORDERER_ADMIN_TLS_PRIVATE_KEY=${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/tls/server.key /dev/null 2>&1
 
 # Create channel 
-peer channel create -o localhost:7053 -c mychannel -f ./channel-artifacts/mychannel.tx --tls --cafile $ORDERER_CA
+#peer channel create -o localhost:7053 -c mychannel -f ./channel-artifacts/mychannel.tx --tls --cafile $ORDERER_CA
 # To join orderer to a channel
 
 #peer channel create -o localhost:7050 -c mychannel — ordererTLSHostnameOverride orderer.example.com -f ./channel-artifacts/mychannel.tx --outputBlock ./channel-artifacts/mychannel.block — tls true --cafile .../fabric-samples/test-network/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
