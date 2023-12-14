b510dd52ad458b911ac6877ea1d7416101978674
nadia800 <147506022+nadiaTalbi@users.noreply.github.com>
2023-12-14 10:17:09 +0100

    Update configUpdate.sh

--- scripts/configUpdate.sh
+++ scripts/configUpdate.sh
@@ -16,11 +16,11 @@ fetchChannelConfig() {
     CHANNEL=$2
     OUTPUT=$3
 
-  setGlobals $ORG 0 
+  setGlobals $ORG
 
   infoln "Fetching the most recent configuration block for the channel"
   set -x
-  peer channel fetch config config_block.pb -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com -c mychannel --tls --cafile "$ORDERER_CA"
+  peer channel fetch config config_block.pb -o orderer.example.com:7050 --ordererTLSHostnameOverride orderer.example.com -c mychannel --tls --cafile "$ORDERER_CA"
   { set +x; } 2>/dev/null
 
   infoln "Decoding config block to JSON and isolating config to ${OUTPUT}"
