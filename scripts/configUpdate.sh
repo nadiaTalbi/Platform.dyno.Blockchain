bf4506292291a46ff6777682c0a4a1191148d44e
nadia800 <147506022+nadiaTalbi@users.noreply.github.com>
2023-12-19 10:27:31 +0100

    Update configUpdate.sh

--- scripts/configUpdate.sh
+++ scripts/configUpdate.sh
@@ -19,7 +19,7 @@ fetchChannelConfig() {
   setGlobals $ORG
 
   export CORE_PEER_TLS_ENABLED=true
-  #export FABRIC_CFG_PATH=/home/dyno/Platform.dyno.Blockchain/compose/docker/peercfg/
+  export $ORDERER_CA=/home/dyno/Platform.dyno.Blockchain/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
 
   infoln "Fetching the most recent configuration block for the channel"
   set -x
