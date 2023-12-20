25df0c49e68bdd215928449327d1263c3f555d7b
nadia800 <147506022+nadiaTalbi@users.noreply.github.com>
2023-12-20 17:44:46 +0100

    update

--- chaincode-external/sampleBuilder/bin/build.sh
+++ chaincode-external/sampleBuilder/bin/build.sh
@@ -1,22 +1,21 @@
 #!/bin/sh
 
-#!/bin/bash
-
 set -euo pipefail
 
-OUTPUT=$3
+SOURCE=../..
+OUTPUT=.
 
 #external chaincodes expect connection.json file in the chaincode package
-if [ ! -f "../../connection.json" ]; then
-    >&2 echo "../../connection.json not found"
+if [ ! -f "$SOURCE/connection.json" ]; then
+    >&2 echo "$SOURCE/connection.json not found"
     exit 1
 fi
 
 #simply copy the endpoint information to specified output location
-cp ../../connection.json ../connection.json
+cp $SOURCE/connection.json $OUTPUT/connection.json
 
-if [ -d "../../metadata" ]; then
-    cp -a ../../metadata ../metadata
+if [ -d "$SOURCE/metadata" ]; then
+    cp -a $SOURCE/metadata $OUTPUT/metadata
 fi
 
 exit 0
